e9fdee952e6ae73ad6ffa6f38046962dfe6fc4b0
Jimmy <an.9647873111@gmail.com>
2023-07-07 17:40:40 +0200

    Cakes_Websate

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const btn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav');
 
 btn.addEventListener('click', ()=>{
-
-})
+    nav.classList.toggle('menu-open');
+});
